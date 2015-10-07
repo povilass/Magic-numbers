ecf0c713259941f6870f55757cca01b60fdecf23
povilass <povilas810@gmail.com>
2015-10-07 08:46:19 +0300

    optimization

--- src/magic/numbers/TestMain.java
+++ src/magic/numbers/TestMain.java
@@ -19,7 +19,7 @@ public class TestMain {
         List<List<String>> result = new LinkedList<>();
         List<Integer> numbers = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-        addBranches(new Sign(), 8, signs, result);
+        addBranches(new Sign(), numbers.size() - 1, signs, result);
 
         printArray(result, numbers);
     }
